2fa99d718110b75bdb86529209bc83d8fe3dca9c
Louis-udm <louis.lv.info@gmail.com>
2019-08-22 15:11:40 -0400

    hierarchical scope role match

--- train_scope_label.py
+++ train_scope_label.py
@@ -1,41 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# # # #
+# train_scope_label.py
+# @author Zhibin.LU
+# @created 2019-08-21T23:35:17.757Z-04:00
+# @last-modified 2019-08-22T15:08:23.610Z-04:00
+# @website: https://louis-udm.github.io
+# @description 
+# Adapting-regulations-for-an-automated-reading-system
+# in The Ninth Montreal Industrial Problem Solving Workshop
+# # # #
+
 #%%
-import os
-
-def set_work_dir(local_path="stage/CRM_workshop", server_path="myprojects/CRM_workshop"):
-    if os.path.exists(os.path.join(os.getenv("HOME"), local_path)):
-        os.chdir(os.path.join(os.getenv("HOME"), local_path))
-    elif os.path.exists(os.path.join(os.getenv("HOME"), server_path)):
-        os.chdir(os.path.join(os.getenv("HOME"), server_path))
-    else:
-        raise Exception('Set work path error!')
-set_work_dir()
-
 import os, pickle
 from glob import glob
 from document import Document, Paragraph, Table
 from utils import *
 
 #%%
+'''
+Define the patterns of scope roles for matcher.
+'''
 
 import spacy
 nlp = spacy.load("en_core_web_lg")
-
-testdoc = nlp(u"""representatives in insurance of persons or insurance representatives are a natural person who offers individual insurance products in insurance of persons or individual annuities from one or more insurers directly to the public, to a firm, to an independent representative or to an independent partnership.
-A representative in insurance of persons is authorized to secure the adhesion of a person in respect of a group insurance or group annuity contract.
-The following are not representatives in insurance of persons and for claims adjuster:
-(1)  persons who, on behalf of an employer, a union, a professional order or an association or professional syndicate constituted under the Professional Syndicates Act (chapter S‐40), secure the adhesion of an employee of that employer or of a member of that union, professional order, association or professional syndicate in respect of a group contract in insurance of persons or a group annuity contract;
-(2)  the members of a mutual benefit association who offer policies for the mutual benefit association.
-We have Insurance Representatives
-and Representative in Insurance of Persons
-and Group Insurance Representatives
-and Damage Insurance agents
-and damage insurance brokers
-and Claims Adjusters
-and financial planners and
-firms and independent representatives and independent partnerships.""".lower())
-
 from spacy.matcher import Matcher
-# from spacy.matcher import PhraseMatcher
 matcher = Matcher(nlp.vocab)
 
 #%%
@@ -65,50 +54,28 @@ if len(matcher)==0:
         matcher.add(r, None, p)
 
 def get_matched_roles(matcher, text):
-    matches = matcher(nlp(text))
+    matches = matcher(nlp(text.lower()))
     return list(set([nlp.vocab.strings[match_id] for match_id, start, end in matches]))
 
-# test
-matches = matcher(testdoc)
-for match_id, start, end in matches:
-    string_id = nlp.vocab.strings[match_id]  # Get string representation
-    span = testdoc[start:end]  # The matched span
-    print(string_id, ':', start, end, span.text)
-
-
-
 #%%
-if False:
-    print('Start label Yifan\'s passages.pkl...')
-    # opening the pickle
-    with open('passages.pkl', 'rb') as fp:
-        passages = pickle.load(fp)
-    for k in passages.keys():
-        text=passages[k]['text']
-        matches = matcher(nlp(text))
-        roles_matched=', '.join(set([nlp.vocab.strings[match_id] for match_id, start, end in matches]))
-        print(k,':',roles_matched)
-        passages[k]['label']=roles_matched
-        # if roles_matched is None or roles_matched.strip()=='':
-        #     print('\n------------------\n',k,':\n',text)
+'''
+Hierarchy documents label processing.
+Label for pkl files .
+'''
 
-    with open('passages_labeled.pkl', 'wb') as fp:
-        pickle.dump(passages,fp)
-#%%
+data_dir = "Distribution de produits et services financiers"
 
-if True:
-    print('Start label hierarchy documents...')
-    print("Reading Loi file in pickle format")
+def label_pkl(filename):
     # opening the pickle
-    with open('Distribution de produits et services financiers/Loi_PICKLE/D-9.2 - Act respecting the distribution of financial products and services.pkl', 'rb') as fp:
+    with open(filename, 'rb') as fp:
         chapter_lst = pickle.load(fp)
-    # chapter_lst[0].section_lst[0].subsection_lst[0][0].text
+    # File written in pkl format are list of chapters
+    # format: chapter_lst[0].section_lst[0].subsection_lst[0][0].text
     for chap_id, chapter in enumerate(chapter_lst):
         # each chapter have a header and section list that
         # can be empty
         header = chapter.header
         section_lst = chapter.section_lst
-        # import pudb;pu.db
         # print header id (e.g. CHAPTER I) and the title (e.g. SPECIAL PROVISION)
         # print(header.tid) #'CHAPTER I'
         # print(header.text) #'GENERAL PROVISIONS'
@@ -133,6 +100,7 @@ if True:
                     section_text+='\n'+subsection.text
 
             section.roles=get_matched_roles(matcher,section_text)
+            # use the roles of father if the section has not roles
             if section.roles is None or len(section.roles)==0:
                 section.roles=header.roles
 
@@ -142,10 +110,90 @@ if True:
             #     print(historical_note.text)
 
             # print("End of Section", "\n")
+    return chapter_lst
+
+print("Reading file in pickle format")
+file_lst = [y for x in os.walk(data_dir+"/Loi_PICKLE") for y in glob(os.path.join(x[0], '*pkl'))]
+files_labeled={}
+for filename in file_lst:
+    print("Label the content of %s" % filename)
+    files_labeled[filename.split('/')[-1]]=label_pkl(filename)
+# with open('chapters_labeled.pkl', 'wb') as fp:
+#     pickle.dump(chapters_labeled,fp)
+
 #%%
-for chap_id, chapter in enumerate(chapter_lst):
-    if chapter.header.roles is not None and len(chapter.header.roles)>0:
-        print(chap_id, chapter.header.tid, chapter.header.roles, chapter.header.text)
-        section_lst = chapter.section_lst
-        for section in section_lst:
-            print(section.roles)
+'''
+Label for Directive_MSWORD .
+'''
+def label_docx(filename):
+    '''
+    for a docx, return a 2-levels list: section->element.
+    every section is a list that has some elements. a element include serval sentences.
+    '''
+
+    def cond(block):
+        if any([run.font.size and run.font.bold for run in block.runs]): return True
+        letter = [c for c in block.text if c.isalpha()]
+        if len(letter) > 15 and len([c for c in letter if c.isupper()]) == len(letter): return True
+        return False
+
+    doc = Document(filename=filename, meta={})
+    sections, par_lst = [], []
+    
+    for idx, block in enumerate(doc.story):
+        if isinstance(block, Paragraph):
+            if cond(block) and len(par_lst) > 4:
+                sections.append(par_lst)
+                par_lst = []
+            if block.text:
+                par_lst.append(block.text)
+
+    if par_lst:
+        sections.append(par_lst)
+    
+    # now we label only for the whole docx document
+    # Can be changed to each section with its labels
+    doc_text=' '.join([' '.join(sec) for sec in sections])
+    roles=get_matched_roles(matcher,doc_text)
+
+    return roles
+
+print("Reading file in docx format")
+file_lst = [y for x in os.walk(data_dir+"/Directive_MSWORD") for y in glob(os.path.join(x[0], '*docx'))]
+docx_labeled={}
+for filename in file_lst:
+    print("Label the content of %s" % filename)
+    docx_labeled[filename.split('/')[-1]]=label_docx(filename)
+# with open('docx_labeled.pkl', 'wb') as fp:
+#     pickle.dump(docx_labeled,fp)
+
+
+#%%
+'''
+Check whether there rest some no-role sections.
+'''
+print('Check whether there rest some no-role sections...')
+for chapter_lst in files_labeled.values():
+    for chap_id, chapter in enumerate(chapter_lst):
+        if chapter.header.roles is not None and len(chapter.header.roles)>0:
+            # import pudb;pu.db
+            section_lst = chapter.section_lst
+            for section in section_lst:
+                # print(section.roles)
+                if section.roles is None or len(section.roles)==0:
+                    section_text=''
+                    # each section is composed of subsection_lst
+                    for subsection in section.subsection_lst:
+                        # a sub section can be a list of paragraph
+                        if isinstance(subsection, list):
+                            for paragraph in subsection:
+                                # print(paragraph.tid, paragraph.text)
+                                section_text+='\n'+paragraph.text
+                        # or a single paragraph
+                        else:
+                            # print(subsection.tid, subsection.text)
+                            section_text+='\n'+subsection.text
+                    print('\n\n',chap_id, chapter.header.tid, chapter.header.roles, chapter.header.text)
+                    print(section.roles, section_text)
+
+print('Finish!')
